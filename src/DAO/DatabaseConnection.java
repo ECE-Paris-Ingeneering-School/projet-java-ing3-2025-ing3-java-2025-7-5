69edebe5ec3dca58b67c779d4a820ee7c703f0ca
cocooooooooooooooooooooo <corentin.chapp@gmail.com>
2025-04-20 10:30:24 +0100

    update database connection URL to point to doctolib

--- src/DAO/DatabaseConnection.java
+++ src/DAO/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/projet_java?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3308/doctolib?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
